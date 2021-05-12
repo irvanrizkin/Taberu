ffb1af642e5d67bdbcfd21d8f5d03f412968637c
Dinasty <aisfarhan@student.ub.ac.id>
2021-05-12 11:12:24 +0700

    Fixing Bugs :)

--- app/src/main/java/com/example/taberu/AddreviewActivity.java
+++ app/src/main/java/com/example/taberu/AddreviewActivity.java
@@ -2,7 +2,9 @@ package com.example.taberu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AddreviewActivity extends AppCompatActivity {
 
@@ -11,4 +13,9 @@ public class AddreviewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addreview);
     }
+    public void pindahsplashactivity(View view){
+        Intent intent = new Intent(this, splashActivity.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/taberu/splashActivity.java
+++ app/src/main/java/com/example/taberu/splashActivity.java
@@ -2,13 +2,27 @@ package com.example.taberu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class splashActivity extends AppCompatActivity {
-
+    Button buttonmulai;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        buttonmulai = findViewById(R.id.splash_button_mulai);
+        buttonmulai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                methodbuttonmulai();
+            }
+        });
+    }
+    public void methodbuttonmulai(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
