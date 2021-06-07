f6556ac9b66f6175c04605f9472fd5b261236052
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2021-06-07 14:50:32 +0700

    stash pop recycler

--- app/src/main/java/com/example/taberu/HomeActivity.java
+++ app/src/main/java/com/example/taberu/HomeActivity.java
@@ -1,9 +1,12 @@
 package com.example.taberu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
@@ -14,11 +17,28 @@ import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class HomeActivity extends AppCompatActivity {
     ImageView optionsImage;
     TextView userLabel;
+    RecyclerView restaurantsRecycler;
     FirebaseAuth firebaseAuth;
     FirebaseUser firebaseUser;
+    RestaurantAdapter.RecyclerListener recyclerListener;
+
+    ArrayList<Restaurant> restaurantList;
+    Retrofit retrofit;
+    RestaurantApi restaurantApi;
+    RestaurantResponse restaurantResponse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +46,12 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         initView();
         initFirebaseAuth();
+        initRetrovit();
+        initRecycler();
 
         setOnClick();
         setDisplayName();
+        getRestaurant();
     }
 
     // View By ID
@@ -82,8 +105,69 @@ public class HomeActivity extends AppCompatActivity {
                 ).show();
                 startActivity(new Intent(HomeActivity.this, LoginActivity.class));
                 return true;
+            } else if (item.getItemId() == R.id.menuitem_profile) {
+                startActivity(new Intent(HomeActivity.this, ProfileActivity.class));
+                return true;
             }
             return false;
         }
     };
+
+    // Retrofit Init
+    private void initRetrovit() {
+        retrofit = new Retrofit.Builder()
+                .baseUrl("https://restaurant-api.dicoding.dev/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        restaurantApi = retrofit.create(RestaurantApi.class);
+    }
+
+    private void getRestaurant() {
+        Call<RestaurantResponse> call = restaurantApi.getRestaurants();
+
+        call.enqueue(new Callback<RestaurantResponse>() {
+            @Override
+            public void onResponse(Call<RestaurantResponse> call, Response<RestaurantResponse> response) {
+                restaurantResponse = response.body();
+                restaurantList = new ArrayList<>(Arrays.asList(restaurantResponse.getRestaurants()));
+
+                showRecycler(restaurantList);
+            }
+
+            @Override
+            public void onFailure(Call<RestaurantResponse> call, Throwable t) {
+                Toast.makeText(getApplicationContext(),
+                        t.getMessage(),
+                        Toast.LENGTH_SHORT
+                ).show();
+            }
+        });
+    }
+
+    //Recycler View
+    private void initRecycler() {
+        restaurantsRecycler = findViewById(R.id.home_recyclerview_recyclerview);
+        restaurantsRecycler.setHasFixedSize(true);
+    }
+
+    private void showRecycler(ArrayList<Restaurant> restaurants) {
+        setRecyclerOnClick();
+        restaurantsRecycler.setLayoutManager(new LinearLayoutManager(
+                getApplicationContext()
+        ));
+        RestaurantAdapter restaurantAdapter = new RestaurantAdapter(restaurants, recyclerListener);
+        restaurantsRecycler.setAdapter(restaurantAdapter);
+    }
+
+    private void setRecyclerOnClick() {
+        recyclerListener = new RestaurantAdapter.RecyclerListener() {
+            @Override
+            public void onClick(View v, int position) {
+                Intent intent = new Intent(getApplicationContext(), RestaurantDetailActivity.class);
+                intent.putExtra("RESTAURANT_ID", restaurantList.get(position).getId());
+                startActivity(intent);
+            }
+        };
+    }
 }
--- app/src/main/java/com/example/taberu/RestaurantDetailActivity.java
+++ app/src/main/java/com/example/taberu/RestaurantDetailActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class RestaurantDetailActivity extends AppCompatActivity {
 
@@ -12,9 +14,24 @@ public class RestaurantDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_detail);
+        TextView titleLabel;
+        ImageView backImage;
+        titleLabel = findViewById(R.id.restaurantdetail_textview_Yoshinoya);
+        backImage = findViewById(R.id.restaurantdetail_imageview_backbutton);
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            titleLabel.setText(bundle.getString("RESTAURANT_ID"));
+        }
+
+        backImage.setOnClickListener(backIntent);
     }
-    public void pindahsplashactivity(View view) {
-        Intent intent = new Intent(this, splashActivity.class);
-        startActivity(intent);
-    }
+
+    public View.OnClickListener backIntent = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+            startActivity(intent);
+        }
+    };
 }
